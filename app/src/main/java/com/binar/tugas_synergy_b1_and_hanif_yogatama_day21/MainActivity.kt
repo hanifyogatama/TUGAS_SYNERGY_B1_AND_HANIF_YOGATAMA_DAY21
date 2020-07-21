48cf212034e4ced638b94bdc4b07c1d52ea632fb
Hanif yogatama <hanifyotama@gmail.com>
2020-07-21 15:10:17 +0700

    done and done

--- app/src/main/java/com/binar/tugas_synergy_b1_and_hanif_yogatama_day21/MainActivity.kt
+++ app/src/main/java/com/binar/tugas_synergy_b1_and_hanif_yogatama_day21/MainActivity.kt
@@ -2,10 +2,17 @@ package com.binar.tugas_synergy_b1_and_hanif_yogatama_day21
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Glide.with(this)
+            .load(R.drawable.oppa)
+            .circleCrop()
+            .into(ivProfile)
     }
 }
